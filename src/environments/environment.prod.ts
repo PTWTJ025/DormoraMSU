f1be5c67ba0b1d15db7f9e34978fa6ec371b721e
PTWTJ025 <phutawan03603@gmail.com>
2026-02-26 15:21:08 +0700

    พ่ีพีพ

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA8u5HYbzrlFm12sNtGTiyzLxwZ2kcS1_o',
+    authDomain: 'projectroomaroo.firebaseapp.com',
+    projectId: 'projectroomaroo',
+    storageBucket: 'projectroomaroo.firebasestorage.app',
+    messagingSenderId: '782979223053',
+    appId: '1:782979223053:web:0ed401a937a53890158f44',
+    measurementId: 'G-X6MZ8F81G1',
+  },
+  // Backend API URLs - Production
+  backendApiUrl: 'https://dormora-msu-back-end.vercel.app/api',
+  // backendApiUrl: 'https://dormroomaroo-backend.onrender.com/api', // ✅ เพิ่ม /api roomaroo
+  // backendApiUrl: 'http://localhost:3000/api', // comment localhost ไว้
+  
+  fastApiUrl: 'https://roomaroo-textclassification.onrender.com/api',
+  mapTilerApiKey: 'Gpwk2Mpi9cl8hUkVrf6f',
+  
+  // Supabase Configuration - Production with new key
+  supabaseUrl: 'https://spismpgbkrpkhedbeevh.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwaXNtcGdya3Jwa2hlZGJlZXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNzQ5MzIsImV4cCI6MjA4NjY1MDkzMn0.XT4c55qK4RfHowNfM2bjnqbXQCu09qD_qeof_pxSvAc',
+};

cf1625f17466f51b942d06362ec04f4886644ce4
PTWTJ025 <phutawan03603@gmail.com>
2026-03-03 02:06:17 +0700

    kkk

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+  // ลบ console.log เฉพาะใน Production
+  if (window) {
+    window.console.log = () => {};
+    window.console.debug = () => {};
+    window.console.info = () => {};
+    // ปล่อย console.error กับ warn ไว้สำหรับเช็คบัคจริง
+  }
+}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch(err => {

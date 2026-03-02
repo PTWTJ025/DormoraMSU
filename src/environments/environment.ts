84943e92c39767db283e7231d02a32fd09da82a5
PTWTJ025 <phutawan03603@gmail.com>
2026-03-03 01:50:40 +0700

    // Dev: console.log // Prod: Sentry/Datadog

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,9 +10,9 @@ export const environment = {
     measurementId: 'G-X6MZ8F81G1',
   },
   // Backend API URLs - สลับระหว่าง localhost และ production
-  backendApiUrl: 'https://dormora-msu-back-end.vercel.app/api', // ✅ เพิ่ม /api
+  // backendApiUrl: 'https://dormora-msu-back-end.vercel.app/api', // ✅ เพิ่ม /api
   // backendApiUrl: 'https://dormroomaroo-backend.onrender.com/api', // ✅ เพิ่ม /api roomaroo
-  // backendApiUrl: 'http://localhost:3000/api', // comment localhost ไว้
+  backendApiUrl: 'http://localhost:3000/api', // comment localhost ไว้
 
   fastApiUrl: 'https://roomaroo-textclassification.onrender.com/api',
   mapTilerApiKey: 'Gpwk2Mpi9cl8hUkVrf6f',
